8643e98987913973c8c1ddbdd24bbface3a1cab7
Peter Hilton <pedro+git@lunatech.com>
2011-10-17 14:50:08 +0200

    Change look-and-feel to single page

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
 
-	$('table.index').dataTable();
+	var section = $('<section/>').addClass('box');
+	section.hide();
+	$('section.box:last').after(section);
 
 	// Details table links.
-	$('table.index tr').click(function() {
-		console.log('click');
+	$('table.index tr').click(function(event) {
+		event.preventDefault();
+
+		// Update list selection.
+		$(this).parent().find('.selected').removeClass('selected');
+		$(this).addClass('selected');
+
+		// Load and display the details.
 		var url = $(this).find('a').attr('href');
-		window.location = url;
+		section.load(url);
+		section.show();
 	});
 
 });
