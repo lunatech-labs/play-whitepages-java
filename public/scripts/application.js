10101c9e231142d2baffcd56cf6b8f858ad5677e
Peter Hilton <pedro+git@lunatech.com>
2011-10-18 15:52:16 +0200

    Make the details box position sticky

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
     // SECTION element for displaying details.
-	var section = $('<section/>').addClass('box');
-	section.hide();
-	$('section.box:last').after(section);
+	var $section = $('<section/>').addClass('box sticky');
+	$section.css('visibility', 'hidden');
+	$('section.box:last').after($section);
 
 	// Details table links.
 	$('.index .entry').live('click', function(event) {
@@ -15,8 +15,8 @@ $(document).ready(function() {
 
 		// Load and display the details.
 		var url = $(this).find('a').attr('href');
-		section.load(url);
-		section.show();
+		$section.load(url);
+    	$section.css('visibility', 'visible');
 	});
 
     // Paging (infinite scroll)
@@ -32,4 +32,15 @@ $(document).ready(function() {
         }
     });
 
+    // Make the details box sticky near the page top.
+    var offset = $section.offset();
+    var width = $section.width();
+    $(window).bind('scroll',function() {
+        if(($('body').scrollTop()) + 24 > offset.top) {
+            $section.css({ position:'fixed', top:'13px', left: offset.left + 'px', width: width + 'px' });
+        }
+        else {
+            $section.css('position', 'static');
+        }
+    });
 });
