ac4c82c4d786e8903831a90c7e833dd37b65d1e6
Peter Hilton <pedro+git@lunatech.com>
2011-10-26 14:59:15 +0200

    Simplify browse by letter

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 
+
    // SECTION element for displaying details.
 	var $section = $('<section/>').addClass('box sticky');
 	$section.css('visibility', 'hidden');
 	$('section.box:last').after($section);
+
 
 	// Details table links.
 	$('.index .entry').live('click', function(event) {
@@ -29,6 +31,29 @@ $(document).ready(function() {
 		});
 	});
 
+
+   // Add letter sections to list.
+
+   function initialLetter($entry) {
+      var entryParagraph = $entry != undefined;
+      return entryParagraph ? $entry.data('file-as').substring(0, 1) : undefined;
+   }
+
+   function addLetters() {
+      var $previous;
+      $('.index p').each(function(index) {
+         var $current = $(this);
+         var twoEntries = ($previous == undefined || $previous.hasClass('entry')) && $current.hasClass('entry');
+         var initial = initialLetter($current);
+         if (twoEntries && initialLetter($previous) != initial) {
+            var $letter = $('<p/>').attr('id', initial.toLowerCase()).addClass('letter').text(initial);
+            $current.before($letter);
+         }
+
+         $previous = $current;
+      });
+   }
+
    // Paging (infinite scroll)
    $('.index').infinitescroll({
       debug: false,
@@ -39,8 +64,10 @@ $(document).ready(function() {
          img: "/assets/scripts/infinite-scroll/ajax-loader.gif",
          msgText: "",
          finishedMsg: "All entries loaded"
-      }
+      },
+      callback: addLetters
    });
+
 
    // Make the details box sticky near the page top.
    var offset = $section.offset();
@@ -54,6 +81,7 @@ $(document).ready(function() {
       }
    });
 
+
    // Enable the search
    $('.search input').keyup(function() {
       var query = $(this).val();
@@ -61,10 +89,12 @@ $(document).ready(function() {
       var url = $(this).closest('form').attr('action');
       var selector = url + '?' + parameter + '=' + query + ' .index p';
       $('.index').load(selector, function() {
+         addLetters();
          $('.index .entry').first().trigger('click');
       });
    });
    $('form.search').submit(function() { return false; });
+
 
    // Select the search form or the entry specified by the fragment identifier, e.g. #p23
    if (location.hash == '') {
@@ -73,4 +103,7 @@ $(document).ready(function() {
    else {
       $(location.hash).trigger('click');
    }
+
+
+   addLetters();
 });
